b86bd658e6e3ce3921f878bb154d5b65df3a8970
Araden14 <arnaud-danselme@hotmail.com>
2024-11-26 15:29:41 +0100

    Add update CV feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { AuthProvider } from './context/AuthContext'; // Assurez-vous du bon chemin
 import Routes from './routes/routes'; // Votre fichier de routes principal
+import EditCV from './pages/CV/EditCV';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <AuthProvider>
       <Routes />
+      <ToastContainer />
     </AuthProvider>
   );
 }
