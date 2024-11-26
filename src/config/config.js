635247e8c03c4151456751c4b3030977d34cf52f
Araden14 <arnaud-danselme@hotmail.com>
2024-11-26 20:07:21 +0100

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,2 @@
-// config.js
-
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8000';
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
+console.log('Backend URL:', process.env.REACT_APP_BACKEND_URL); // For debugging
